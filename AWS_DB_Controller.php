096b665e88bc6f15aea958e4ed487c4bf1a9119a
Justin Greene <justingreene@Justins-iMac.local>
2018-06-15 14:47:30 -0600

    updated for new database instance

--- AWS_DB_Controller.php
+++ AWS_DB_Controller.php
@@ -13,7 +13,7 @@ if(isset($_GET["sqlquery"])) {
   $qstring = $_GET["sqlquery"];
 }
 
-$host = 'mytunes.c2kfwcjpb96b.us-west-1.rds.amazonaws.com'; //host address
+$host = 'mytunes1-2.c2kfwcjpb96b.us-west-1.rds.amazonaws.com'; //host address
 $user = 'Master'; //change to user name for database
 $pass = '1234outfd'; //change to password for database
 $dbName = 'MyTunes1.0'; // change to database name
