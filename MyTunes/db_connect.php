c2b46c193023b444f033ef21f7b25cbecebba6d0
Justin Greene <justingreene@Justins-iMac.local>
2018-06-25 21:01:49 -0600

    added delete functionality

--- MyTunes/db_connect.php
+++ MyTunes/db_connect.php
@@ -1,14 +1,25 @@
 <?php
 
+//testing Colben's database
+
 // $host = '127.0.0.1'; //host address
 // $user = 'tester'; //change to user name for database
 // $pass = '1234outfd'; //change to password for database
-// $dbName = 'MyTunes2.0'; // change to database name
+// $dbName = 'MyTunes'; // change to database name
 
-$host = 'mytunes1-2.c2kfwcjpb96b.us-west-1.rds.amazonaws.com'; //host address
-$user = 'Master'; //change to user name for database
+//testing my local db
+
+$host = '127.0.0.1'; //host address
+$user = 'tester'; //change to user name for database
 $pass = '1234outfd'; //change to password for database
 $dbName = 'MyTunes2.0'; // change to database name
+
+//testing AWS 
+
+// $host = 'mytunes1-2.c2kfwcjpb96b.us-west-1.rds.amazonaws.com'; //host address
+// $user = 'Master'; //change to user name for database
+// $pass = '1234outfd'; //change to password for database
+// $dbName = 'MyTunes2.0'; // change to database name
 
 //create connection
 $connection = mysqli_connect($host, $user, $pass, $dbName);
---
+++ MyTunes/deleteTrack.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-eqiv="X-UA-Compatible" content="IE-edge">
+  <meta name="viewport" content="width-device-width, initial-scale=1">
+  <title>Successful Update</title>
+  <link href="bootstrap-3.3.7-dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body style="margin:16px;padding:16px">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+  <script src="bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
+<?php
+
+require_once('db_connect.php');
+
+$id = mysqli_real_escape_string($connection,$_POST['track_id']);
+
+$sql = "DELETE FROM TRACK WHERE TrackID = '$id'";
+if(mysqli_query($connection,$sql)){
+  echo "Record deleted Successfully.";
+  echo "<br />";
+  echo "<a class='btn btn-success' href='MyTunes_index.html'>Return Home</a>";
+} else {
+  echo "ERROR: Cound not execute sql." . mysqli_error($connection);
+}
+mysqli_close($connection);
+
+ ?>
+</body>
+</html>
